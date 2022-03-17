4b4720f81707fa074476afefedc79a40697ba14b
Stevin Wilson <stevinw@clemson.edu>
2022-03-17 14:00:27 -0400

    documentation for get_daily_diagnostics()

--- R/get_daily_diagnostics.R
+++ R/get_daily_diagnostics.R
@@ -1,13 +1,15 @@
 #' Get Daily Diangostics Counts
 #'
-#' @param diagnostics_tbl
+#' @param diagnostics_tbl Tibble containing the following columns 1)testkit_id,
+#' 2) run_Date
+#' 3)result
 #' @param start_date Filter sample collection table for samples collected after
 #' this date. Provide date as in "2021-08-01" for August 1, 2021
 #' @param end_date Filter sample collection table for samples collected on or
 #' before this date. Provide date as in "2021-08-01" for August 1, 2021
 #' @param add_variables variables to group_by
 #'
-#' @return
+#' @return Tibble with daily counts of different diagnostics result categories
 #' @export
 #'
 #' @importFrom magrittr "%>%"
@@ -26,6 +28,12 @@ get_daily_diagnostics <- function(diagnostics_tbl,
   start_date <- lubridate::as_date(start_date)
   end_date <- lubridate::as_date(end_date)
 
+  diagnostics_tbl <- diagnostics_tbl %>%
+    dplyr::filter(
+      .data$run_date >= start_date,
+      .data$run_date <= end_date
+    )
+
   grouping_variable <- c(
     "run_date",
     "result"
@@ -38,19 +46,15 @@ get_daily_diagnostics <- function(diagnostics_tbl,
     )
   }
 
-  diagnostics_tbl <- diagnostics_tbl %>%
-    dplyr::filter(
-      .data$run_date >= start_date,
-      .data$run_date <= end_date
-    )
-
   count_tbl <- diagnostics_tbl %>%
     dplyr::group_by(grouping_variable) %>%
-    dplyr::summarise(count = dplyr::n())
+    dplyr::summarise(count = dplyr::n()) %>%
+    dplyr::ungroup()
 
   repeat_samples <- diagnostics_tbl %>%
     dplyr::group_by(.data$testkit_id) %>%
-    dplyr::filter(dplyr::n() > 1)
+    dplyr::filter(dplyr::n() > 1) %>%
+    dplyr::ungroup()
 
   output_list <- list(
     count_tbl = count_tbl,
