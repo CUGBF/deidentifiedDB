2feb274900d43ea2eb31f358d63270ba17d12a7d
Stevin Wilson <stevinw@clemson.edu>
2022-03-17 18:02:31 -0400

    get_daily_diagnostics()

--- R/get_daily_diagnostics.R
+++ R/get_daily_diagnostics.R
@@ -1,4 +1,4 @@
-#' Get Daily Diangostics Counts
+#' Get Daily Diagnostics Counts
 #'
 #' @param diagnostics_tbl Tibble containing the following columns 1)testkit_id,
 #' 2) run_Date
@@ -7,7 +7,7 @@
 #' this date. Provide date as in "2021-08-01" for August 1, 2021
 #' @param end_date Filter sample collection table for samples collected on or
 #' before this date. Provide date as in "2021-08-01" for August 1, 2021
-#' @param add_variables variables to group_by
+#' @param ... Names of columns to group_by
 #'
 #' @return Tibble with daily counts of different diagnostics result categories
 #' @export
@@ -17,8 +17,10 @@
 get_daily_diagnostics <- function(diagnostics_tbl,
                                   start_date,
                                   end_date,
-                                  add_variables = NULL) {
-  stopifnot(all(c("testkit_id", "run_date", "result") %in% colnames(diagnostics_tbl)))
+                                  ...) {
+  stopifnot(all(c("testkit_id", "run_date", "result",
+                  "control") %in% colnames(diagnostics_tbl)))
+
 
   if (!(lubridate::is.Date(diagnostics_tbl$run_date))) {
     diagnostics_tbl <- diagnostics_tbl %>%
@@ -29,25 +31,17 @@ get_daily_diagnostics <- function(diagnostics_tbl,
   end_date <- lubridate::as_date(end_date)
 
   diagnostics_tbl <- diagnostics_tbl %>%
+    dplyr::mutate(control = as.logical(control)) %>%
     dplyr::filter(
+      !.data$control,
       .data$run_date >= start_date,
       .data$run_date <= end_date
     )
 
-  grouping_variable <- c(
-    "run_date",
-    "result"
-  )
-
-  if (!is.null(add_variables)) {
-    grouping_variable <- c(
-      grouping_variable,
-      add_variables
-    )
-  }
+  # https://stackoverflow.com/questions/57704792/is-it-possible-to-pass-multible-variables-to-the-same-curly-curly
 
   count_tbl <- diagnostics_tbl %>%
-    dplyr::group_by(grouping_variable) %>%
+    dplyr::group_by(!!!dplyr::ensyms(...)) %>%
     dplyr::summarise(count = dplyr::n()) %>%
     dplyr::ungroup()
 
--- tests/testthat/test-get_daily_diagnostics.R
+++ tests/testthat/test-get_daily_diagnostics.R
@@ -1,3 +1,161 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Testing get_daily_diagnostics() !", {
+  sample_diagnostics <- system.file("extdata/daily_diagnostics",
+    "sample_diagnostics_tbl.csv",
+    package = "deidentifiedDB"
+  )
+
+  diagnostics_tbl <- readr::read_csv(sample_diagnostics,
+    show_col_types = FALSE
+  )
+
+  # Case I
+  output_list <- get_daily_diagnostics(
+    diagnostics_tbl = diagnostics_tbl,
+    start_date = "2022-03-10",
+    end_date = "2022-03-20",
+    run_date,
+    result
+  )
+
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-16",
+        result == "RERUN",
+      ) %>%
+      dplyr::pull(count),
+    1
+  )
+
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-16",
+        result == "POSITIVE",
+      ) %>%
+      dplyr::pull(count),
+    3
+  )
+
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-16",
+        result == "NEGATIVE",
+      ) %>%
+      dplyr::pull(count),
+    3
+  )
+
+  # Case II
+  output_list <- get_daily_diagnostics(
+    diagnostics_tbl = diagnostics_tbl,
+    start_date = "2022-03-10",
+    end_date = "2022-03-20",
+    run_date,
+    result,
+    machine
+  )
+
+  expect_equal(
+    nrow(output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-15",
+        result == "POSITIVE",
+        machine == "Tim",
+      )) ,
+    0
+  )
+
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+           dplyr::filter(
+             run_date == "2022-03-15",
+             result == "NEGATIVE",
+             machine == "Tim",
+           ) %>%
+      dplyr::pull(count),
+    1
+  )
+
+
+  # Case III
+  output_list <- get_daily_diagnostics(
+    diagnostics_tbl = diagnostics_tbl,
+    start_date = "2022-03-10",
+    end_date = "2022-03-20",
+    run_date,
+    result,
+    machine,
+    thermocycler
+  )
+
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-17",
+        result == "POSITIVE",
+        machine == "Tim",
+        thermocycler == "R2"
+      ) %>%
+      dplyr::pull(count),
+    1
+  )
+
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-16",
+        result == "RERUN",
+        machine == "Tim",
+        thermocycler == "R1"
+      ) %>%
+      dplyr::pull(count),
+    1
+  )
+
+  expect_equal(
+    nrow(output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        run_date == "2022-03-17",
+        result == "NEGATIVE",
+        machine == "Tim",
+        thermocycler == "R2"
+      )),
+    0
+  )
+
+  # Case IV
+  output_list <- get_daily_diagnostics(
+    diagnostics_tbl = diagnostics_tbl,
+    start_date = "2022-03-10",
+    end_date = "2022-03-20",
+    mastermix,
+    result
+  )
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        mastermix == "XYZ",
+        result == "NEGATIVE",
+      ) %>%
+      dplyr::pull(count),
+    13
+  )
+  expect_equal(
+    output_list[["count_tbl"]] %>%
+      dplyr::filter(
+        mastermix == "XYZ",
+        result == "POSITIVE",
+      ) %>%
+      dplyr::pull(count),
+    5
+  )
+  expect_true(
+    all(output_list[["repeat_samples"]] %>%
+      dplyr::pull(
+        testkit_id
+      ) %>%
+      unique() %in% c('B', 'H', 'K')
+  ))
 })
