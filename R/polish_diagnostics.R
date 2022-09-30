8b8c20f50b8101adbcf5e6a3ddf4de04cdb1fcb2
Rooksana E Noorai <Rooksie@users.noreply.github.com>
2022-09-30 14:28:06 -0400

    Revert "Reverting changes"
    This reverts commit ea3a1d9515631be46c602356d2aeebc33f0ecb24.

--- R/polish_diagnostics.R
+++ R/polish_diagnostics.R
@@ -16,35 +16,47 @@ polish_diagnostics <- function(diagnostics_tbl, run_date_fmt = c("mdy")) {
     "ct_N_rep1",
     "ct_N_rep2",
     "result",
-    "machine",
-    "thermocycler",
-    "pcr_type",
     "plate"
   ) %in% colnames(diagnostics_tbl)))
 
-@@ -35,10 +32,7 @@ polish_diagnostics <- function(diagnostics_tbl, run_date_fmt = c("mdy")) {
+  if (grepl("-", diagnostics_tbl$run_date[1])) {  
+    output_tbl <- diagnostics_tbl %>%
+      dplyr::mutate(
+        run_date = lubridate::date(.data$run_date),
+        control = detect_control(.data$testkit_id, .data$result),
+        dplyr::across(tidyselect::vars_select_helpers$where(is.character), stringr::str_trim),
+        dplyr::across(c(
+          .data$ct_rnasep_rep1,
+          .data$ct_rnasep_rep2,
+          .data$ct_N_rep1,
           .data$ct_N_rep2
         ), tidy_up_ct),
         dplyr::across(c(
-          .data$result,
-          .data$machine,
-          .data$thermocycler,
-          .data$pcr_type
+          .data$result
         ), stringr::str_to_lower),
         plate = as.numeric(.data$plate)
       ) %>%
-@@ -56,10 +50,7 @@ polish_diagnostics <- function(diagnostics_tbl, run_date_fmt = c("mdy")) {
+      dplyr::arrange(.data$run_date)
+  } else {
+    output_tbl <- diagnostics_tbl %>%
+      dplyr::mutate(
+        run_date = lubridate::date(lubridate::parse_date_time(.data$run_date, orders = run_date_fmt)),
+        control = detect_control(.data$testkit_id, .data$result),
+        dplyr::across(tidyselect::vars_select_helpers$where(is.character), stringr::str_trim),
+        dplyr::across(c(
+          .data$ct_rnasep_rep1,
+          .data$ct_rnasep_rep2,
+          .data$ct_N_rep1,
           .data$ct_N_rep2
         ), tidy_up_ct),
         dplyr::across(c(
-          .data$result,
-          .data$machine,
-          .data$thermocycler,
-          .data$pcr_type
+          .data$result
         ), stringr::str_to_lower),
         plate = as.numeric(.data$plate)
       ) %>%
       dplyr::arrange(.data$run_date)
   }
+
+
   return(output_tbl)
 }
