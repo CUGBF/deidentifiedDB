ca1c8892e70355a5710fc0ccb5d5f29d0e9a76b9
Rooksana E Noorai <Rooksie@users.noreply.github.com>
2023-02-21 09:57:20 -0500

    Update tidy_up_race.R
    Put back five '.date$'

--- R/tidy_up_race.R
+++ R/tidy_up_race.R
@@ -12,31 +12,31 @@ tidy_up_race <- function(demographics_tbl) {
   output_tbl <- demographics_tbl %>%
     dplyr::mutate(
       race_white = stringr::str_detect(
-        race,
+        .data$race,
         stringr::regex("White",
           ignore_case = T
         )
       ),
       race_asian = stringr::str_detect(
-        race,
+        .date$race,
         stringr::regex("Asian",
           ignore_case = T
         )
       ),
       race_black_or_african_american = stringr::str_detect(
-        race,
+        .date$race,
         stringr::regex("Black|African",
           ignore_case = T
         )
       ),
       race_american_indian_or_alaskan_native = stringr::str_detect(
-        race,
+        .date$race,
         stringr::regex("Indian|Alaska",
           ignore_case = T
         )
       ),
       race_native_hawaiian_or_pacific_islander = stringr::str_detect(
-        race,
+        .date$race,
         stringr::regex("Hawaiian|Pacific|Islander",
           ignore_case = T
         )
